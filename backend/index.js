7e6e530ff5162197ccb1eb763c7007d501e599b0
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-09-18 12:25:17 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require( 'express' );
+const app = express();
+
+app.get( '/', function( req, res ) {
+    res.send( 'Hola mundo' )
+} )
+
+app.listen( 3000,()=>console.log('Servidor levantado en puerto 3000') )
